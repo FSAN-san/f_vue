6bbb74530bbd0b27b9e8271eb75c7a09fd15c015
FSAN-san <1507393930@qq.com>
2022-06-15 09:08:14 +0800

    Vue3 + Vite + ESLint + Vue Router + Vuex + PostCSS + SCSS

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <p>Hello {{ name }}</p>
+    <p>This is Vue3 + Vite + Eslint + {{ state.routerStore.name }} + Vuex + PostCSS + SCSS</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useStore } from 'vuex'
+
+const { commit, state } = useStore()
+commit('printName')
+const name = 'FSAN'
+</script>
+
+<style scoped lang="scss">
+div {
+  font-size: 40px;
+  display: grid;
+
+  p {
+    font-weight: bold;
+    display: flex;
+  }
+}
+</style>
